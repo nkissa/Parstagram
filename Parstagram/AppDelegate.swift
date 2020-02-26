029444530b19639969032a3cebf0abdeec7c552d
Nasar Issa <nkissa@ucsd.edu>
2020-02-26 12:39:37 -0800

    Added feature that allows the user to stay logged in

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -11,7 +11,8 @@ import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+//variable for window
+//var window:UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
