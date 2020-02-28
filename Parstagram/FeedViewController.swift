6a0877e2f71ffdc03c01476cb45a951585c3bdc0
Nasar Issa <nkissa@ucsd.edu>
2020-02-27 19:49:50 -0800

    Added Add a comment functionality to Instagram

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -14,11 +14,14 @@ import MessageInputBar
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MessageInputBarDelegate{
     
+    
     @IBOutlet weak var tableView: UITableView!
     let commentBar = MessageInputBar()
-    var showsCommentBar = true
+    var showsCommentBar = false
     
     var posts = [PFObject]()
+    var selectedPost: PFObject!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -66,6 +69,21 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
         //Create the comment
+        let comment = PFObject(className: "Comments")
+        comment["text"] = text
+        comment["post"] = selectedPost
+        comment["author"] = PFUser.current()!
+        
+        selectedPost.add(comment, forKey: "comments")
+        selectedPost.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved")
+            }
+            else{
+                print("Error saving comment")
+            }
+        }
+        tableView.reloadData()
         //Clear adn dismiss the input bar
         commentBar.inputTextView.text = nil
         showsCommentBar = false
@@ -122,29 +140,16 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let post = posts[indexPath.row]
+        let post = posts[indexPath.section]
         let comments = (post["comments"] as? [PFObject]) ?? []
         
         if indexPath.row == comments.count + 1 {
             showsCommentBar = true
             becomeFirstResponder()
             commentBar.inputTextView.becomeFirstResponder()
-            
+            selectedPost = post
         }
         
-        /*comment["text"] = "This is a random comment"
-        comment["post"] = post
-        comment["author"] = PFUser.current()!
-        
-        post.add(comment, forKey: "comments")
-        post.saveInBackground { (success, error) in
-            if success {
-                print("Comment saved")
-            }
-            else{
-                print("Error saving comment")
-            }
-        }*/
         
     }
     
